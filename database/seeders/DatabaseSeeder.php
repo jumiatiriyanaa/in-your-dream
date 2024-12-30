0900ec26b001b8baa2346a9ead4d672109d4cc75
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2024-12-31 00:44:52 +0700

    fix: update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 use Database\Seeders\AdminSeeder;
 use Database\Seeders\PackagesSeeder;
+use Database\Seeders\BackgroundSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +18,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
-            PackagesSeeder::class,
+            // PackagesSeeder::class,
+            // BackgroundSeeder::class,
         ]);
     }
 }
