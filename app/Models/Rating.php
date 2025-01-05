8bb375782854d299c423cde8767664a48e32b1fc
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2025-01-05 17:08:36 +0700

    fix: update models

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Reservation;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Rating extends Model
 {
