fdf2f02834ab2c88bd1544f07d2bb290daa92766
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2025-01-09 18:40:52 +0700

    fix: add link card dashboard admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -22,121 +22,139 @@
                 <div class="row">
                     <!-- Reservasi Card -->
                     <div class="col-xxl-4 col-md-6">
-                        <div class="card info-card sales-card">
-                            <div class="card-body">
-                                <h5 class="card-title">
-                                    Reservasi
-                                </h5>
-                                <div class="d-flex align-items-center">
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                        <i class="bi bi-credit-card-2-back"></i>
-                                    </div>
-                                    <div class="ps-3">
-                                        <h6>{{ $totalReservation }}</h6>
+                        <a href="{{ route('admin.reservations.index') }}" class="text-decoration-none">
+                            <div class="card info-card sales-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Reservasi
+                                    </h5>
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-credit-card-2-back"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>{{ $totalReservation }}</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <!-- End Reservasi Card -->
 
                     <!-- Gallery Card -->
                     <div class="col-xxl-4 col-md-6">
-                        <div class="card info-card customers-card">
-                            <div class="card-body">
-                                <h5 class="card-title">
-                                    Gallery
-                                </h5>
-                                <div class="d-flex align-items-center">
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                        <i class="bi bi-image"></i>
-                                    </div>
-                                    <div class="ps-3">
-                                        <h6>{{ $totalGalleries }}</h6>
+                        <a href="{{ route('admin.galleries.index') }}" class="text-decoration-none">
+                            <div class="card info-card customers-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Gallery
+                                    </h5>
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-image"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>{{ $totalGalleries }}</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <!-- End Gallery Card -->
 
                     <!-- Photographer Card -->
                     <div class="col-xxl-4 col-md-6">
-                        <div class="card info-card customers-card">
-                            <div class="card-body">
-                                <h5 class="card-title">
-                                    Photographer
-                                </h5>
-                                <div class="d-flex align-items-center">
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                        <i class="bi bi-person-gear"></i>
-                                    </div>
-                                    <div class="ps-3">
-                                        <h6>{{ $totalUsersLevel0 }}</h6>
+                        <a href="{{ route('admin.photographers.index') }}" class="text-decoration-none">
+                            <div class="card info-card customers-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Photographer
+                                    </h5>
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-person-gear"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>{{ $totalUsersLevel0 }}</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <!-- End Photographer Card -->
 
                     <!-- Background Card -->
                     <div class="col-xxl-4 col-md-6">
-                        <div class="card info-card customers-card">
-                            <div class="card-body">
-                                <h5 class="card-title">
-                                    Background
-                                </h5>
-                                <div class="d-flex align-items-center">
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                        <i class="bi bi-images"></i>
-                                    </div>
-                                    <div class="ps-3">
-                                        <h6>{{ $totalBackgrounds }}</h6>
+                        <a href="{{ route('admin.backgrounds.index') }}" class="text-decoration-none">
+                            <div class="card info-card customers-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Background
+                                    </h5>
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-images"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>{{ $totalBackgrounds }}</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <!-- End Background Card -->
 
                     <!-- Package Card -->
                     <div class="col-xxl-4 col-md-6">
-                        <div class="card info-card revenue-card">
-                            <div class="card-body">
-                                <h5 class="card-title">
-                                    Package
-                                </h5>
-                                <div class="d-flex align-items-center">
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                        <i class="bi bi-plus-square"></i>
-                                    </div>
-                                    <div class="ps-3">
-                                        <h6>{{ $totalPackages }}</h6>
+                        <a href="{{ route('admin.packages.index') }}" class="text-decoration-none">
+                            <div class="card info-card revenue-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Package
+                                    </h5>
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-plus-square"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>{{ $totalPackages }}</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <!-- End Package Card -->
 
                     <!-- About Us Card -->
                     <div class="col-xxl-4 col-md-6">
-                        <div class="card info-card sales-card">
-                            <div class="card-body">
-                                <h5 class="card-title">
-                                    About Us
-                                </h5>
-                                <div class="d-flex align-items-center">
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                        <i class="bi bi-info-circle"></i>
-                                    </div>
-                                    <div class="ps-3">
-                                        <h6>{{ $totalAboutUs }}</h6>
+                        <a href="{{ route('admin.about-us.index') }}" class="text-decoration-none">
+                            <div class="card info-card sales-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        About Us
+                                    </h5>
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-info-circle"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>{{ $totalAboutUs }}</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <!-- End About Us Card -->
                 </div>
